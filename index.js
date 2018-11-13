ecf9a79d8e397822ade8208037976d9503f7624a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-13 00:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Driver {
   }
   
   passengers() {
-    console.log(this.trips())
+    console.log(this.trips().map())
     return this.trips().map(
       function(trip) {
         return trip.passenger()
